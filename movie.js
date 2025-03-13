7409ebd8d90d6d314cefef9a252c2292333269f2
kenny <khaneesh50@gmail.com>
2025-03-13 20:44:56 +0530

    removed bad server

--- movie.js
+++ movie.js
@@ -110,7 +110,7 @@ function setupServerSwitchers() {
 function updatePlayer(server) {
   const playerIframe = document.getElementById("player-iframe");
   const servers = {
-    server1: `https://embed.su/embed/movie/${id}`, // Embed.su
+    server1: `https://vidsrc.icu/embed/movie/${id}`, // vidsrc.icu
     server2: `https://vidlink.pro/movie/${id}`, // vidlink
     server3: `https://vidsrc.to/embed/movie/${id}`, //vidsrc
   };
--- tv.js
+++ tv.js
@@ -252,7 +252,7 @@ function updatePlayer(server, episodeNumber = 1) {
   const seasonNumber = seasonSelect.value;
 
   const servers = {
-    server1: `https://embed.su/embed/tv/${id}/${seasonNumber}/${episodeNumber}`, // Embed.su
+    server1: `https://vidsrc.icu/embed/tv/${id}/${seasonNumber}/${episodeNumber}`, // vidsrc.icu
     server2: `https://vidlink.pro/tv/${id}/${seasonNumber}/${episodeNumber}`, // vidlink
     server3: `https://vidsrc.to/embed/tv/${id}/${seasonNumber}/${episodeNumber}`, //vidsrc
   };
